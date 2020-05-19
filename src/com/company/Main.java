46dc7533c253b2d6b6baf28ff90305cee3410396
Diana <diana.yakushevaa@gmail.com>
2020-05-19 21:52:14 +0300

    la6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,92 @@
 package com.company;
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+@SuppressWarnings("serial")
+public class Main extends JFrame {
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+    private JMenuItem pauseMenuItem;
+    private JMenuItem resumeMenuItem;
+    private JMenuItem pauMenuItem;
+    private JMenuItem resMenuItem;
 
-public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private Field field = new Field();
+    public Main() {
+        super("Программирование и синхронизация потоков");
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - WIDTH)/2, (kit.getScreenSize().height - HEIGHT)/2);
+        setExtendedState(MAXIMIZED_BOTH);
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu ballMenu = new JMenu("Мячи");
+        Action addBallAction = new AbstractAction("Добавить мяч") {
+            public void actionPerformed(ActionEvent event) {
+                field.addBall();
+                if (!pauseMenuItem.isEnabled() &&
+                        !resumeMenuItem.isEnabled()) {
+                    pauseMenuItem.setEnabled(true);
+                }
+                }
+        };
+        menuBar.add(ballMenu);
+        ballMenu.add(addBallAction);
+        JMenu controlMenu = new JMenu("Управление");
+        menuBar.add(controlMenu);
+        Action pauseAction = new AbstractAction("Приостановить движение"){
+            public void actionPerformed(ActionEvent event) {
+                field.pause();
+                pauseMenuItem.setEnabled(false);
+                resumeMenuItem.setEnabled(true);
+            }
+        };
+        pauseMenuItem = controlMenu.add(pauseAction);
+        pauseMenuItem.setEnabled(false);
+        Action resumeAction = new AbstractAction("Возобновить движение") {
+            public void actionPerformed(ActionEvent event) {
+                field.resume();
+                pauseMenuItem.setEnabled(true);
+                resumeMenuItem.setEnabled(false);
+            }
+        };
+        resumeMenuItem = controlMenu.add(resumeAction);
+        resumeMenuItem.setEnabled(false);
+
+        Action pauAction = new AbstractAction("Приостановить выборочно движение"){
+            public void actionPerformed(ActionEvent event) {
+                field.paused();
+                pauMenuItem.setEnabled(false);
+                resumeMenuItem.setEnabled(true);
+            }
+        };
+        pauMenuItem = controlMenu.add(pauAction);
+        pauMenuItem.setEnabled(true);
+
+        Action resAction = new AbstractAction("Возобновить  выборочно движение") {
+            public void actionPerformed(ActionEvent event) {
+                field.resumed();
+                pauMenuItem.setEnabled(true);
+                resMenuItem.setEnabled(false);
+            }
+        };
+        resMenuItem = controlMenu.add(resAction);
+        resMenuItem.setEnabled(false);
+
+
+        getContentPane().add(field, BorderLayout.CENTER);
+    }
+    public static void main(String[] args) {  Main frame = new Main();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
     }
 }
+
+
